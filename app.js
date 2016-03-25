da023bfdbbc17ed666ad7530ca1838ea289839b2
stefholbrook <stefholbrook@gmail.com>
2016-03-25 22:26:07 +0000

    Add site pages

---
+++ app.js
@@ -0,0 +1,6 @@
+var main = function() {
+     var cities = ['Tokyo', 'Seoul', 'Mexico City', 'New York', 'Mumbai', 'Jakarta', 'Sao Paulo', 'Delhi', 'Keihanshin', 'Shanghai', 'Manila', 'Hong Kong', 'Los Angeles', 'Kolkata', 'Moscow', 'Cairo', 'Buenos Aires', 'London', 'Beijing', 'Karachi']; 
+  
+     $( "#search" ).autocomplete({source: cities }); 
+ }; 
+$(document).ready(main); 
